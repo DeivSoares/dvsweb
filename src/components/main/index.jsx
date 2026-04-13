9083745c66d4ccc43144340872596953567d74cd
Deivison Soares <99440916+DeivSoares@users.noreply.github.com>
2026-04-13 19:59:30 -0300

    Updates
    Alterado textos, Stacks, rootColors, nav e adicionado aba de Projetos

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -3,6 +3,7 @@ import Logo from "../../assets/icons/DvsLogo.png";
 import Perfil from "../../assets/icons/perfil.png";
 import Social from '../social';
 import Button from '../button';
+import ProjetosData from '../projetos/projetosData';
 
 function Main() {
   // Carrega automaticamente os ícones da pasta src/assets/icons
@@ -29,9 +30,10 @@ function Main() {
     { name: "CSS", icon: loadIcon("CSS") },
     { name: "JavaScript", icon: loadIcon("JavaScript") },
     { name: "React", icon: loadIcon("React") },
-    { name: "C#", icon: loadIcon("CSharp") },
+    { name: "NodeJS", icon: loadIcon("NodeJS") },
     { name: "Git", icon: loadIcon("Git") },
     { name: "GitHub", icon: loadIcon("GitHub") },
+    { name: "C#", icon: loadIcon("CSharp") },
   ];
 
   return (
@@ -44,37 +46,52 @@ function Main() {
             Deivison Soares
           </h1>
 
-          <h2>Transformo sua ideia em realidade</h2>
+          <h2>Transformando ideias em experiências digitais</h2>
           <p>
-            A DVS Web é uma empresa especializada no desenvolvimento de sites modernos, funcionais e personalizados, focada em transformar ideias em presença digital de qualidade.
+            A DVS Web é um projeto especializado no desenvolvimento de sites modernos, funcionais e personalizados, com foco em transformar ideias em uma presença digital de qualidade.
             <br></br>
-            Mais do que criar páginas, entregamos soluções digitais alinhadas com a visão de cada cliente, prezando sempre pela clareza, eficiência e qualidade na execução.
+            Mais do que criar páginas, buscamos oferecer soluções digitais alinhadas com a visão de cada cliente, prezando pela clareza, eficiência e qualidade em cada entrega.
           </p>
         </div>
 
         <div className="intro-image">
-          <img src={Perfil} alt="Imagem de Perfil Deivison Soares"/>
+          <img src={Perfil} alt="Imagem de Perfil Deivison Soares" />
           <Social />
           <Button />
         </div>
-      </section>
 
+        
       <section className="stacks-section">
         <h3>Minhas Stacks</h3>
         <div className="stacks-grid">
           {stacks.map((stack, index) => (
             <div key={index} className="stack-item">
               <div className="stack-icon">
-                {typeof stack.icon === "string" &&
-                  stack.icon.startsWith("/") ? (
-                  <img src={stack.icon} alt={`${stack.name} icon`} />
-                ) : (
-                  <span>{stack.icon}</span>
-                )}
+                {typeof stack.icon === "string" && stack.icon.startsWith("/") ? (<img src={stack.icon} alt={`${stack.name} icon`} />) : (<span>{stack.icon}</span>)}
               </div>
               <span className="stack-name">{stack.name}</span>
             </div>
           ))}
+        </div>
+      </section>
+
+
+      </section>
+
+
+      <section className="projects">
+        <h3>Meus Projetos</h3>
+        <div className="projects-grid">
+          {ProjetosData.map((projeto, index) => (
+            <div key={index} className="project-item">
+              <h4>{projeto.title}</h4>
+              <p>{projeto.description}</p>
+              <a href={projeto.link} target="_blank" rel="noopener noreferrer">
+                <img src={projeto.image} alt={projeto.title} />
+              </a>
+            </div>
+          ))}
+
         </div>
       </section>
     </main>
---
+++ src/components/projetos/projetosData.js
@@ -0,0 +1,46 @@
+const ProjetosData = [
+    {
+        title: "Psicoterapia Ativa",
+        description: "Desenvolvi o site Psicoterapia Ativa para oferecer uma interface intuitiva e moderna, focada em promover os serviços de psicoterapia. O site apresenta uma navegação fácil, design responsivo e conteúdo informativo, visando proporcionar uma experiência agradável aos visitantes e destacar a importância da saúde mental.",
+        image: "https://deivsoares.github.io/Portfolio/src/images/Psicoterapia%20Ativa.jpeg",
+        link: "#",
+    },
+    {
+        title: "Elemental Wiki",
+        description: "Esse é um projeto interno que desenvolvi para o Elemental Priston Tale! O Elemental Wiki é uma plataforma de conhecimento colaborativa, projetada para reunir informações, guias e recursos relacionados ao jogo. O objetivo é criar um espaço onde os jogadores possam compartilhar dicas, estratégias e detalhes sobre o universo do Elemental Priston Tale, promovendo a interação e o engajamento da comunidade.",
+        image: "https://deivsoares.github.io/Portfolio/src/images/Elemental%20Wiki.png",
+        link: "#",
+    },
+    {
+        title: "Decodificador de Texto",
+        description: "Projeto desenvolvido durante o curso da Oracle; O Decodificador de Texto é uma aplicação que utiliza técnicas de criptografia para transformar mensagens em códigos indecifráveis. Ele permite que os usuários insiram texto e escolham um método de codificação, garantindo a segurança e privacidade das informações. Ideal para proteger dados sensíveis ou criar mensagens secretas.",
+        image: "https://deivsoares.github.io/Portfolio/src/images/decodificador.png",
+        link: "#",
+    },
+    {
+        title: "Relógio Digital",
+        description: "Relogio Digital em tempo real; O Relógio Digital é uma aplicação que exibe a hora atual de forma clara e estilizada. Ele utiliza JavaScript para atualizar o horário em tempo real, oferecendo uma interface moderna e fácil de ler. Ideal para ser usado como um widget em sites ou como um projeto de aprendizado para entender manipulação de data e hora em programação.",
+        image: "https://deivsoares.github.io/Portfolio/src/images/relogio-digital.png",
+        link: "#",
+    },
+    {
+        title: "Site com Login e Registro",
+        description: "Projeto desenvolvido apenas para fixar o aprendizado; O Site com Login e Registro é uma aplicação web que permite aos usuários criar contas, fazer login e acessar conteúdo exclusivo. Ele inclui funcionalidades de autenticação, validação de dados e gerenciamento de sessões, proporcionando uma experiência segura e personalizada para os usuários. Ideal para aprender sobre segurança e gerenciamento de usuários em desenvolvimento web.",
+        image: "https://deivsoares.github.io/Portfolio/src/images/Site%20com%20login%20e%20registro.png",
+        link: "#",
+    },
+    {
+        title: "The Last of Us - Série de TV",
+        description: "Projeto desenvolvido durante o evento do Zero ao Programador Contratado - Devemdobro; O The Last of Us - Série de TV é um projeto de desenvolvimento web que apresenta informações, trailers e notícias sobre a série de TV baseada no famoso jogo. O site oferece uma interface atraente e fácil de navegar, permitindo aos fãs do jogo e da série se manterem atualizados sobre os lançamentos e detalhes relacionados à produção.",
+        image: "https://deivsoares.github.io/Portfolio/src/images/deivsoares.github.io_the-last-of-us_%20(2).png",
+        link: "#",
+    },
+    {
+        title: "NLW eSports",
+        description: "Projeto desenvolvido durante o evento NLW eSports da RocketSeat; O NLW eSports é um projeto de desenvolvimento web focado em criar uma plataforma para entusiastas de esportes eletrônicos. O site apresenta informações estilo perfil de usuário, jogos favoritos, canais e streamers favoritos e redes sociais, oferecendo uma experiência interativa e envolvente. Ideal para aprender sobre desenvolvimento front-end e integração de APIs relacionadas a jogos.",
+        image: "https://deivsoares.github.io/Portfolio/src/images/preview2.png",
+        link: "#",
+    },
+]
+
+export default ProjetosData;
