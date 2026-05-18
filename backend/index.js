729b6c20fbd47af0e57585464f65670fd374d935
Deivison Soares <99440916+DeivSoares@users.noreply.github.com>
2026-05-18 17:12:04 -0300

    up

--- backend/index.js
+++ backend/index.js
@@ -3,19 +3,56 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const dashboardRoutes = require("./routes/dashboard");
+const dashboardRoutes =
+  require("./routes/dashboard");
 
-const clientesRoutes = require("./routes/clientes");
+const clientesRoutes =
+  require("./routes/clientes");
 
-const botsRoutes = require("./routes/bots");
+const botsRoutes =
+  require("./routes/bots");
 
-const atividadesRoutes = require("./routes/atividades");
+const atividadesRoutes =
+  require("./routes/atividades");
 
 const app = express();
 
-app.use(cors());
-
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+    methods: [
+      "GET",
+      "POST",
+      "PUT",
+      "DELETE",
+    ],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+    ],
+  })
+);
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "*"
+  );
+
+  res.header(
+    "Access-Control-Allow-Headers",
+    "*"
+  );
+
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE"
+  );
+
+  next();
+});
 
 app.get("/", (req, res) => {
   res.json({
@@ -28,8 +65,9 @@ app.use("/clientes", clientesRoutes);
 app.use("/bots", botsRoutes);
 app.use("/atividades", atividadesRoutes);
 
-const PORT = process.env.PORT || 3001;
+const PORT =
+  process.env.PORT || 3001;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("API ONLINE");
 });
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://subheader-clock-shadily.ngrok-free.dev",
+  baseURL:
+    "https://subheader-clock-shadily.ngrok-free.dev",
+
+  headers: {
+    "ngrok-skip-browser-warning":
+      "true",
+  },
 });
