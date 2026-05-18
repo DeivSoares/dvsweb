ce911c7b358d40bb66ff711581a7c9e7c2bf39e5
Deivison Soares <99440916+DeivSoares@users.noreply.github.com>
2026-05-18 17:32:45 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,18 +22,16 @@ app.use(express.json());
 app.use(
   cors({
     origin: "*",
-    methods: [
-      "GET",
-      "POST",
-      "PUT",
-      "DELETE",
-    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: [
       "Content-Type",
       "Authorization",
+      "ngrok-skip-browser-warning",
     ],
   })
 );
+
+app.options("*", cors());
 
 app.use((req, res, next) => {
   res.header(
