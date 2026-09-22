e04bb8062b7fb9523653aa81d4d3a38b89a4e784
Deivison Soares <99440916+DeivSoares@users.noreply.github.com>
2026-09-22 14:20:46 -0300

    Update Bots.jsx

--- src/pages/Painel/Bots.jsx
+++ src/pages/Painel/Bots.jsx
@@ -167,6 +167,7 @@ export default function Bots() {
               </div>
 
               <p>{bot.descricao}</p>
+              <br></br>
 
               <strong>
                 Mensalidade: R$ {Number(bot.valorMensal ?? bot.valor ?? 0).toFixed(2)}
